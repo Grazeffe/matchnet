8a1a0921da77a8a2ce49ec6a37aba5eb42556fa8
Valdir Grazeffe Junior <Grazeffe@users.noreply.github.com>
2019-09-28 14:38:52 -0300

    starting repo

---
+++ src/main/java/com/netshoes/matchnet/MatchnetApplication.java
@@ -0,0 +1,13 @@
+package com.netshoes.matchnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchnetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchnetApplication.class, args);
+	}
+
+}
